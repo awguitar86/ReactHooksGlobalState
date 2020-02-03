7fed7cc6912659421100064218db99bf5d5a2854
awguitar86 <awguitar86@gmail.com>
2020-02-03 14:30:36 -0700

    Finished section on using custom reach hooks for global state.

--- src/components/Products/ProductItem.js
+++ src/components/Products/ProductItem.js
@@ -1,16 +1,19 @@
-import React, {useContext} from 'react';
+import React from 'react';
 
 import Card from '../UI/Card';
 import './ProductItem.css';
-import { toggleFav } from '../../store/actions/products';
-import { ProductsContext} from '../../context/products-context'
+import { useStore } from '../../hooks-store/store'
+// import { toggleFav } from '../../store/actions/products';
+// import { ProductsContext} from '../../context/products-context'
 
-const ProductItem = props => {
-
-  const toggleFav = useContext(ProductsContext).toggleFav
+const ProductItem = React.memo(props => {
+  console.log('Render')
+  const dispatch = useStore(false)[1]
+  // const toggleFav = useContext(ProductsContext).toggleFav
 
   const toggleFavHandler = () => {
-    toggleFav(props.id)
+    // toggleFav(props.id)
+    dispatch('TOGGLE_FAV', props.id)
   };
 
   return (
@@ -27,6 +30,6 @@ const ProductItem = props => {
       </div>
     </Card>
   );
-};
+});
 
 export default ProductItem;
--- src/hooks-store/store.js
+++ src/hooks-store/store.js
@@ -1,17 +1,41 @@
 import { useState, useEffect } from 'react'
 
+// Since these are defined outside of the useStore hook every component can share and use all of the data from all the components.
+// This allows us to manage state globally.
 let globalState = {}
 let listeners = []
 let actions = {}
 
-const useStore = () => {
-  const setState = useState(globalState)[1];
+export const useStore = (shouldListen = true) => {
+  const setState = useState(globalState)[1]; //the [1] here means the second value in teh array useState gives us, which is the set function setState.
+
+  const dispatch = (actionIdentifier, payload) => {
+    const newState = actions[actionIdentifier](globalState, payload)
+    globalState = { ...globalState, ...newState }
+
+    for(const listener of listeners) {
+      listener(globalState)
+    }
+  }
 
   useEffect(() => {
-    listeners.push(setState)
+    if(shouldListen) {
+      listeners.push(setState)
+    }
 
     return () => {
-      listeners = listeners.filter(li => li !== setState)
+      if(shouldListen) {
+        listeners = listeners.filter(li => li !== setState)
+      }
     }
-  }, [setState])
+  }, [setState, shouldListen])
+
+  return [ globalState, dispatch ]
+}
+
+export const initStore = (userActions, initialState) => {
+  if(initialState) {
+    globalState = { ...globalState, ...initialState }
+  }
+  actions = { ...actions, ...userActions }
 }
